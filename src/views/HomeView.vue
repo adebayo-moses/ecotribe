b2baa639bea1ce50c105aab5844dd265f667af94
Moses Adebayo <mosesadebayo@Mosess-MacBook-Pro.local>
2022-11-10 23:17:54 +0100

    build: core components, header and section complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,110 @@
 <template>
-    <div class="text-white mx-8 md:mx-auto">
-        <p>Boilerplate to get you started with your <code>vue</code>3 apps🎉.</p>
-        <p class="mb-4">It comes configured with <code>vue</code>3, <code>tailwind</code> and <code>vue-router</code></p>
+  <div>
+    <div class="relative">
+      <header class="absolute inset-x-0 top-0 z-10 w-full">
+        <div class="px-4 mx-auto sm:px-6 lg:px-8">
+          <div class="flex items-center justify-between h-16 lg:h-20">
+            <div class="flex-shrink-0">
+              <a href="#" title="" class="flex">
+                <img
+                  class="w-auto h-8"
+                  src="../assets/ecotribe-logo-light.png"
+                  alt="The Eco Tribe Logo"
+                />
+              </a>
+            </div>
 
-        <p>why boilerplate?</p>
-        <p>I got tired of having to install and configure <code>tailwind</code> for my <code>vue</code>3 side projects😅.</p>
+            <button
+              type="button"
+              class="inline-flex p-2 text-black transition-all duration-200 rounded-md lg:hidden focus:bg-gray-100 hover:bg-gray-100"
+            >
+              <!-- Menu open: "hidden", Menu closed: "block" -->
+              <svg
+                class="block w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+
+              <!-- Menu open: "block", Menu closed: "hidden" -->
+              <svg
+                class="hidden w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+
+            <div
+              class="hidden ml-auto lg:flex lg:items-center lg:justify-center lg:space-x-10"
+            >
+              <a
+                href="#"
+                title=""
+                class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"
+              >
+                Solutions
+              </a>
+
+              <a
+                href="#"
+                title=""
+                class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"
+              >
+                Who We Are
+              </a>
+
+              <a
+                href="#"
+                title=""
+                class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"
+              >
+                Updates
+              </a>
+
+              <a
+                href="#"
+                title=""
+                class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"
+              >
+                Contact Us
+              </a>
+
+              <a
+                href="#"
+                title=""
+                class="inline-flex items-center justify-center px-5 py-2.5 text-base font-semibold transition-all duration-200 rounded-full bg-green-500 text-white hover:bg-green-600 focus:bg-green-600"
+                role="button"
+              >
+                Join our Group
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
+  </div>
+  <HeroAside></HeroAside>
 </template>
 
 <script setup>
-
+import HeroAside from '../components/HeroAside.vue';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
